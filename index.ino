6492208a8d57252d6d2def884e370ee4d9d9efdf
richie-south <richard.soderman@gmail.com>
2017-10-17 22:04:09 +0200

    feat: using new water level sensor, shut down program if valve is open for 8 or more secs

--- index.ino
+++ index.ino
@@ -1,6 +1,9 @@
 #include <Servo.h>
+#include <elapsedMillis.h>
 
 Servo myservo;
+elapsedMillis timeElapsed;
+
 const int analogInPin = A0;
 const int minWaterLevel = 280;
 const int maxWaterLevel = 300;
@@ -8,44 +11,59 @@ const int maxWaterLevel = 300;
 const int valveClosedPos = 93;
 const int valveFullyOpen = 10;
 
+const int checkIntervall = 10000;
+const int catShouldDrinkUpDelay = (1000 * 60) * 8; // delay that minimizes the risk of cat drinking water while refilling
+const int valveOpenInterval = 8 * 1000; // max time the valve should be open
+boolean isActive = true; // ability to "stop" the program
+const int in = 4;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(in, in);
+
   myservo.attach(9);
   myservo.write(valveClosedPos);
 }
 
-boolean isWaterUnderMinLevel(int minLevel) {
-  int sensorValue = analogRead(analogInPin);
-  Serial.println(sensorValue);
-  return sensorValue <= minLevel;
+boolean isWaterUnderMinLevel() {
+  int rd = digitalRead(in);
+  return rd == HIGH;
 }
 
 void loop() {
-  if (isWaterUnderMinLevel(minWaterLevel)) {
-    Serial.println("isWaterUnderMinLevel: true");
-    delay(15000);
+  if (!isActive) {
+    return false;
+  }
 
-    if (isWaterUnderMinLevel(minWaterLevel)) {
+  if (isWaterUnderMinLevel()) {
+    Serial.println("isWaterUnderMinLevel: true");
+    delay(checkIntervall);
+
+    if (isWaterUnderMinLevel()) {
       Serial.println("again isWaterUnderMinLevel: true");
       Serial.println("open valve");
+
+      timeElapsed = 0;
       myservo.write(valveFullyOpen + 40);
 
-      while(isWaterUnderMinLevel(maxWaterLevel)) {
+
+      while(isWaterUnderMinLevel() && timeElapsed < valveOpenInterval) {
         Serial.println("while isWaterUnderMinLevel: true");
+      }
+
+      if (timeElapsed >= valveOpenInterval) {
+        Serial.println("Time has gone over board!");
+        isActive = false;
       }
 
       Serial.println("close valve");
       myservo.write(valveClosedPos);
     }
+  } else {
+
+    Serial.println("was not under min level");
   }
-  delay(15000);
-  // get water sensor level
 
-  // if level is under 140, wait for 5 minutes
-
-  // check water level again
-  // if level is under 140, open servo
-  // read water level every 100mm
-  // when water level is over 140 close servo
-
+  timeElapsed = 0;
+  delay(checkIntervall);
 }
