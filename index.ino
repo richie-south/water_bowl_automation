c2f37ce2e8965b362048f9707bac8458d99ef9ca
richie-south <richard.soderman@gmail.com>
2017-10-18 17:51:32 +0200

    refactor: removed logs

--- index.ino
+++ index.ino
@@ -12,13 +12,12 @@ const int valveClosedPos = 93;
 const int valveFullyOpen = 10;
 
 const int checkIntervall = 10000;
-const int catShouldDrinkUpDelay = (1000 * 60) * 8; // delay that minimizes the risk of cat drinking water while refilling
-const int valveOpenInterval = 8 * 1000; // max time the valve should be open
+const unsigned long catShouldDrinkUpDelay = 480000; // 8 min, delay that minimizes the risk of cat drinking water while refilling
+const int valveOpenInterval = 7000; // max time the valve should be open
 boolean isActive = true; // ability to "stop" the program
 const int in = 4;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(in, in);
 
   myservo.attach(9);
@@ -36,32 +35,22 @@ void loop() {
   }
 
   if (isWaterUnderMinLevel()) {
-    Serial.println("isWaterUnderMinLevel: true");
-    delay(checkIntervall);
+    delay(catShouldDrinkUpDelay);
 
     if (isWaterUnderMinLevel()) {
-      Serial.println("again isWaterUnderMinLevel: true");
-      Serial.println("open valve");
 
       timeElapsed = 0;
       myservo.write(valveFullyOpen + 40);
 
-
       while(isWaterUnderMinLevel() && timeElapsed < valveOpenInterval) {
-        Serial.println("while isWaterUnderMinLevel: true");
       }
 
       if (timeElapsed >= valveOpenInterval) {
-        Serial.println("Time has gone over board!");
         isActive = false;
       }
 
-      Serial.println("close valve");
       myservo.write(valveClosedPos);
     }
-  } else {
-
-    Serial.println("was not under min level");
   }
 
   timeElapsed = 0;
